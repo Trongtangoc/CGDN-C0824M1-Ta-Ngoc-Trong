906d7a4586d6f7b4a794fae98faef619ca35d5ba
Trongtangoc <lenhisomot@gmail.com>
2024-10-03 08:20:54 +0700

    update bai tap

--- ss7/baitap/bien_kieudulieu_toantu.js
+++
@@ -1,9 +0,0 @@
-
-function tinhDiemtrungbinh(){
-    let diemToan = +prompt("Nhập điểm Toán");
-    let diemLy = +prompt("Nhập điểm Lý");
-    let diemHoa = +prompt("Nhập điểm Hóa");
-    let TB = (diemToan + diemHoa + diemLy) / 3;
-    document.getElementById("show diem trung binh").innerHTML = "Điểm trung bình là: " + TB + "<br>" + "Điểm toán là: " + diemToan + "<br>" + "Điểm Lý là: " + diemLy+ "<br>" + "Điểm Hóa là: " + diemHoa;
-
-}
