62c7c20c80eb9f7d6d52ab61cb01c45c0eaf13ad
Trongtangoc <lenhisomot@gmail.com>
2025-02-28 19:51:33 +0700

    laptop

--- Case-Study/src/ManagerContact.java
+++ Case-Study/src/ManagerContact.java
@@ -10,7 +10,7 @@ public class ManagerContact {
 
 
     public void addContact(Contact contact) {
-        if(!isValidPhoneNumber(contact.getPhoneNumber()) || !isValidEmail(contact.getEmail())){
+        if (!isValidPhoneNumber(contact.getPhoneNumber()) || !isValidEmail(contact.getEmail())) {
             throw new IllegalArgumentException("Invalid phone number or email");
         }
         contacts.add(contact);
@@ -25,43 +25,49 @@ public class ManagerContact {
         return phoneNumber.matches("^[0-9]{10}$");
     }
 
-    public void updateContact() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter contact number: ");
-        String updatePhoneNumber = scanner.nextLine();
-        Contact updateContact = null;
-        for (Contact contact : contacts) {
-            if (contact.getPhoneNumber().equals(phoneNumber)) {
-                updateContact = contact;
-                break;
-            }
-        }
-        if (updateContact == null) {
-            System.out.println("Contact not found");
-            return;
-        }
-        System.out.println("Update Contact ");
-        System.out.println("Update New Contact ");
-        updateContact.setPhoneNumber(updatePhoneNumber);
-        updateContact.setEmail(updatePhoneNumber);
-        updateContact.setName(updatePhoneNumber);
-        updateContact.setAddress(updatePhoneNumber);
-        updateContact.setBirthday(updatePhoneNumber);
-        updateContact.setGender(updatePhoneNumber);
+    public void updateContact(String updatePhone, String updateGroup, String updateName, String updateGender, String updateAddress, String updateBirthDate, String updateEmail) {
 
     }
+
+    //    public void updateContact() {
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("Enter contact number: ");
+//        String updatePhoneNumber = scanner.nextLine();
+//        Contact updateContact = null;
+//        for (Contact contact : contacts) {
+//            if (contact.getPhoneNumber().equals(phoneNumber)) {
+//                updateContact = contact;
+//                break;
+//            }
+//        }
+//        if (updateContact == null) {
+//            System.out.println("Contact not found");
+//            return;
+//        }
+//        System.out.println("Update Contact ");
+//        System.out.println("Update New Contact ");
+//        updateContact.setPhoneNumber(updatePhoneNumber);
+//        updateContact.setEmail(updatePhoneNumber);
+//        updateContact.setName(updatePhoneNumber);
+//        updateContact.setAddress(updatePhoneNumber);
+//        updateContact.setBirthday(updatePhoneNumber);
+//        updateContact.setGender(updatePhoneNumber);
+//
+//    }
     public void deleteContact(String phoneNumber) {
         contacts.removeIf(contact -> contact.getPhoneNumber().equals(phoneNumber));
         System.out.println("Deleted contact");
     }
+
     public void searchContact(String query) {
         for (Contact contact : contacts) {
-          if (contact.getPhoneNumber().equals(query) || contact.getName().toLowerCase().contains(query.toLowerCase()))  {
-              System.out.println(contact);
-          }
+            if (contact.getPhoneNumber().equals(query) || contact.getName().toLowerCase().contains(query.toLowerCase())) {
+                System.out.println(contact);
+            }
         }
         System.out.println("Contact found");
     }
+
     public void displayContacts() {
         if (contacts.size() > 0) {
             for (Contact contact : contacts) {
@@ -73,6 +79,7 @@ public class ManagerContact {
 
 
     }
+
     public void sortContacts() {
         contacts.sort(Comparator.comparing(Contact::getName));
     }
@@ -80,7 +87,7 @@ public class ManagerContact {
     public void loadFromFile() {
         //Check file exist or not?
         System.out.println("File does it exist? " + new File(FILE_PATH).exists());
-        System.out.println("The file has "+ new File(FILE_PATH).length() + " bytes");
+        System.out.println("The file has " + new File(FILE_PATH).length() + " bytes");
         //warning ở đây
         Scanner scanner = new Scanner(System.in);
         System.out.println("Cảnh báo: Lựa chọn “Đọc từ File” sẽ thực hiện xoá toàn bộ danh bạ đang có trong bộ nhớ.");
@@ -92,11 +99,11 @@ public class ManagerContact {
         }
         //reset dữ liệu trong lịch sử
         contacts.clear();
-        try (BufferedReader br = new BufferedReader(new FileReader(FILE_PATH))){
+        try (BufferedReader br = new BufferedReader(new FileReader(FILE_PATH))) {
 
             String line = null;
             boolean firstLine = true;
-            while ((line = br.readLine()) != null){
+            while ((line = br.readLine()) != null) {
                 if (firstLine) {
                     firstLine = false;
                     continue;
@@ -114,7 +121,7 @@ public class ManagerContact {
 
     public void saveToFile() {
 
-        try (BufferedWriter bw = new BufferedWriter(new FileWriter(FILE_PATH))){
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(FILE_PATH))) {
             bw.write("PhoneNumber,Group,Name,Gender,Address,BirthDate,Email");
             bw.newLine();
             for (Contact contact : contacts) {
@@ -122,10 +129,11 @@ public class ManagerContact {
                 bw.newLine();
             }
 
-        }
-        catch (IOException e) {
+        } catch (IOException e) {
             System.out.println("Error writing file" + e.getMessage());
             e.printStackTrace();
         }
     }
+
+
 }
